ace90657ea36b1769c0d5fb0aeb71e423558444f
fatemeh asgari <fatemeh.asgari@tapsi.cab>
2022-12-09 19:55:28 +0330

    Add src and dest inputs

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-$('#navbar').load('/components/navbar.html');
+$("#navbar").load("/components/navbar.html");
+
+function removePlaceHolder(input, event) {
+  if (event.type == "focus") {
+    input.setAttribute("rel", input.getAttribute("placeholder"));
+    input.removeAttribute("placeholder");
+  }
+
+  if (event.type == "blur") {
+    input.setAttribute("placeholder", input.getAttribute("rel"));
+    input.removeAttribute("rel");
+  }
+}
