8b427818f7d8fbf514abca3c4bbb69d174cbbcc2
Pouya Esmaili <poesmaili@gmail.com>
2022-12-12 13:17:44 +0330

    Add header and footer to navbar

--- auth/script.js
+++ auth/script.js
@@ -1,3 +1,6 @@
+$("#navbar").load("/components/navbar.html");
+$("#footer").load("/components/footer.html");
+
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
